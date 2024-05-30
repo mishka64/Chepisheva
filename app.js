54dcc03b9d54f528374c3bc84d12c6107b95159d
mishka64 <148968768+mishka64@users.noreply.github.com>
2024-05-30 11:47:32 +0200

    d

--- app.js
+++ app.js
@@ -3,9 +3,13 @@
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
+async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  const posts = await getPosts();
+  console.log(posts);
 }
 async function getPosts(){
-  const response = await fetch("http://zofiakubisz.magdalenapietras.dk/wp-json/wp/v2/posts?_acf_format=standard")
+  const response = await fetch("http://zofiakubisz.magdalenapietras.dk/wp-json/wp/v2/posts?_acf_format=standard");
+  const data = await response.json();
+  return data;
 }

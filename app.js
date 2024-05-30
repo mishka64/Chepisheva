8de131c4e4f7082fc0354dfecafbb9916f42e6f3
mishka64 <148968768+mishka64@users.noreply.github.com>
2024-05-30 17:22:40 +0200

    f

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
-"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
-
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
 async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
-  const posts = await getPosts();
-  console.log(posts);
+  const projects = await getProjects(); // Corrected the function name
+  console.log(projects);
+  // displayProjects(projects);
+  displayProjectsGrid(projects);
 }
-async function getPosts(){
-  const response = await fetch("http://zofiakubisz.magdalenapietras.dk/wp-json/wp/v2/posts?_acf_format=standard");
+
+async function getProjects() {
+  const response = await fetch(
+    "http://exam.cafesite.dk/wp-json/wp/v2/project?acf_format=standard"
+  );
   const data = await response.json();
   return data;
+}
+
+function displayProjects(projects) {
+  const projectsList = document.querySelector("#project-list");
+
+  for (const project of projects) {
+    // Corrected the parameter name in the for...of loop
+    projectsList.insertAdjacentHTML(
+      "beforeend",
+      `
+      <li>${project.title.rendered} </li>
+      `
+    );
+  }
+}
+
+function displayProjectsGrid(projects) {
+  const projectsGrid = document.querySelector("#project-grid");
+  for (const project of projects) {
+    // Corrected the parameter name in the for...of loop
+    projectsGrid.insertAdjacentHTML(
+      "beforeend",
+      `    
+    <article class="grid-item">
+      <img src="${project.acf.image}" alt="${project.title.rendered}"></img>
+      <h2>${project.title.rendered}</h2>
+      <p>${project.acf.description}</p>
+     <p>${project.acf.client}</p>
+    
+       <p>${project.acf.type}</p>
+  <p><a href="${project.acf.link}" target="_blank">View Project</a></p>
+    </article>
+    `
+    );
+  }
 }
